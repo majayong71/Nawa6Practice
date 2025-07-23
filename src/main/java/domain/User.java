3293fa0e8a20a6ea14754eb8d91a87d4d280b5fc
majayong71 <wotkd2647@naver.com>
2025-07-23 18:43:52 +0900

    domain-user developing

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,9 +1,6 @@
 package domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Generated;
 import lombok.Getter;
@@ -18,6 +15,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column (length = 50)
+    private String email;
 
 
 }
