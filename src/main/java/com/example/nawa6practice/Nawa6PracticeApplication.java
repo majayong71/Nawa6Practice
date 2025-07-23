a4d3d01b6c08f1ec2003acdc0e71873a9c95020e
majayong71 <wotkd2647@naver.com>
2025-07-23 17:58:43 +0900

    first commit

---
+++ src/main/java/com/example/nawa6practice/Nawa6PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nawa6practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nawa6PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nawa6PracticeApplication.class, args);
+    }
+
+}
