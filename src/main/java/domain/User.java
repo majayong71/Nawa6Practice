340a2f4c3a35098ed28d8d9ca77aa38f8a9ec076
majayong71 <wotkd2647@naver.com>
2025-07-23 18:08:50 +0900

    first commit

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,4 +1,21 @@
 package domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
