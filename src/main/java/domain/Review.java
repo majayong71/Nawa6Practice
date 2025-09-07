a22ffed5994b405cd0a61dd33445796b037a52c1
majayong71 <wotkd2647@naver.com>
2025-09-07 23:33:12 +0900

    feat: Add Review domain

--- src/main/java/domain/Review.java
+++ src/main/java/domain/Review.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @AllArgsConstructor
@@ -16,14 +18,73 @@ public class Review {
     private Long id;
 
     /**
+     * 유저
      * 여러 리뷰가 하나의 유저를 참조
      */
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     /**
+     * 레스토랑
      * 여러 리뷰가 하나의 레스토랑을 참조
      */
     @ManyToOne(fetch = FetchType.LAZY)
     private Restaurant restaurant;
+
+    /**
+     * 내용
+     */
+    @Column(length = 200)
+    private String content;
+
+    /**
+     * 이미지
+     */
+    @Column(length = 200)
+    private String imageUrl;
+
+    /**
+     * 점수
+     **/
+    @Column(length = 5)
+    private int rating;
+
+    /**
+     * 상태
+     **/
+    @Enumerated(EnumType.STRING)
+    private ReviewStatus status = ReviewStatus.REGISTERED;
+
+    /**
+     * 생성 일시
+     **/
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    /**
+     * 삭제 일시
+     **/
+    private LocalDateTime deletedAt;
+
+    public Review(User user, Restaurant restaurant, String content, String imageUrl, int rating) {
+        this.user = user;
+        this.restaurant = restaurant;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+    }
+
+    public void update(String content, String imageUrl, int rating) {
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+    }
+
+    public void deleted() {
+        if (this.status != ReviewStatus.REGISTERED) {
+            throw new IllegalStateException("check fail");
+        }
+
+        this.status = ReviewStatus.DELETED;
+        this.deletedAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/domain/ReviewStatus.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum ReviewStatus {
+    REGISTERED, // 등록됨
+    DELETED     // 삭제됨
+}
