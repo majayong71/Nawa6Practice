882cbfbf4d1214077fdd06f7053457dea4371cda
majayong71 <wotkd2647@naver.com>
2025-09-08 15:45:43 +0900

    feat: Add Comment domain

---
+++ src/main/java/domain/Comment.java
@@ -0,0 +1,78 @@
+package domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 유저
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    /**
+     * 게시글
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    /**
+     * 내용
+     */
+    private String content;
+
+    /**
+     * 이미지
+     */
+    private String imageUrl;
+
+    /**
+     * 상태
+     */
+    @Enumerated(EnumType.STRING)
+    private CommentStatus status = CommentStatus.REGISTERED;
+
+    /**
+     * 생성 일시
+     */
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    /**
+     * 삭제 일시
+     */
+    private LocalDateTime deletedAt;
+
+    public Comment(User user, Post post, String content, String imageUrl) {
+        this.user = user;
+        this.post = post;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public void update() {
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public void deleted() {
+        if (this.status != CommentStatus.REGISTERED) {
+            throw new IllegalStateException("check fail");
+        }
+
+        this.status = CommentStatus.DELETED;
+        this.deletedAt = LocalDateTime.now();
+    }
+}
