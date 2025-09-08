b229b39e07a3e037933d09f4010277e49dfc07a6
majayong71 <wotkd2647@naver.com>
2025-09-08 15:52:47 +0900

    feat: Add Banner domain developing

---
+++ src/main/java/domain/Banner.java
@@ -0,0 +1,22 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Banner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
