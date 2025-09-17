c766d7d3ac9f7c6e519011eb394bf2d74d733856
majayong71 <wotkd2647@naver.com>
2025-09-17 19:11:22 +0900

    feat: Bookmark service & impl & repository ing

--- src/main/java/repository/BookmarkRepository.java
+++ src/main/java/repository/BookmarkRepository.java
@@ -1,7 +1,11 @@
 package repository;
 
 import domain.Bookmark;
+import domain.BookmarkStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    List<Bookmark> findBybookmarkStatus(BookmarkStatus status);
 }
---
+++ src/main/java/service/BookmarkService.java
@@ -0,0 +1,18 @@
+package service;
+
+import domain.Banner;
+import domain.Bookmark;
+
+import java.util.List;
+
+public interface BookmarkService {
+
+    /** 북마크 전체 조회 **/
+    List<Bookmark> gets();
+
+    /** 북마크 등록 **/
+    void register(Long userId, Long postId);
+
+    /** 북마크 삭제 **/
+    void delete(Long id);
+}
