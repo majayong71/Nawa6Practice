912a2ab7c3355aaa757c0de3daa9e55a7e71d980
majayong71 <wotkd2647@naver.com>
2025-09-23 15:54:57 +0900

    feat: PostController start

--- src/main/java/com/example/nawa6practice/controller/PostController.java
+++ src/main/java/com/example/nawa6practice/controller/PostController.java
@@ -1,5 +1,12 @@
 package com.example.nawa6practice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/post")
 public class PostController {
 
 }
