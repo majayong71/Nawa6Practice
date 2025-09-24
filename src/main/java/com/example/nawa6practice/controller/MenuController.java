a7c6f857323ea0f739e46281f1a85ea9103bedcc
majayong71 <wotkd2647@naver.com>
2025-09-25 03:40:27 +0900

    feat: MenuService start

--- src/main/java/com/example/nawa6practice/controller/MenuController.java
+++ src/main/java/com/example/nawa6practice/controller/MenuController.java
@@ -1,4 +1,15 @@
 package com.example.nawa6practice.controller;
 
+import com.example.nawa6practice.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/menus")
 public class MenuController {
+    private final MenuService menuService;
+
+
 }
--- src/main/java/com/example/nawa6practice/controller/PostController.java
+++ src/main/java/com/example/nawa6practice/controller/PostController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostController {
 
     private final PostService postService;
