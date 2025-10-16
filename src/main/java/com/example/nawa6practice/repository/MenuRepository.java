f8e0cc3dc3c0ca236ef48404c959e4a0f2fc7c6b
majayong71 <wotkd2647@naver.com>
2025-10-16 14:28:32 +0900

    refactor : MenuRepository Query

--- src/main/java/com/example/nawa6practice/repository/MenuRepository.java
+++ src/main/java/com/example/nawa6practice/repository/MenuRepository.java
@@ -3,11 +3,37 @@ package com.example.nawa6practice.repository;
 import com.example.nawa6practice.domain.Menu;
 import com.example.nawa6practice.domain.MenuStatus;
 import com.example.nawa6practice.domain.Restaurant;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-
+    @Query("""
+                    SELECT m FROM Menu m
+                    WHERE m.restaurant = :restaurant AND m.status = :status
+                    ORDER BY m.isMain DESC, m.id ASC
+            
+            """)
     List<Menu> findByRestaurantAndStatus(Restaurant restaurant, MenuStatus status);
+
+    @Query("""
+            SELECT m FROM Menu m
+            WHERE m.id = :id AND m.status = 'ACTIVE'
+            ORDER BY m.isMain DESC, m.id ASC
+            """)
+    @NonNull
+    Optional<Menu> findById(@NonNull Long id);
+
+    /**
+     *     또는
+     *     @Query ("""
+     *             SELECT m FROM Menu m
+     *             WHERE m.id = :id AND m.status = 'ACTIVE'
+     *             ORDER BY m.isMain DESC, m.id ASC
+     *             """)
+     *     Optional<Menu> findActiveMenuById(Long id);
+     */
 }
