9f121195da60b6ccbc680aae9645f98fafeb1833
majayong71 <wotkd2647@naver.com>
2025-10-19 00:34:45 +0900

    refactor : PostRepository Query

--- src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
+++ src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
     List<Restaurant> findByCategory(RestaurantCategory category);
 
     Long id(Long id);
