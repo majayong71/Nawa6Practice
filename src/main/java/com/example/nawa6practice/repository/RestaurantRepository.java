2bdfe26274eca6570e488dfee651104d5ff369aa
majayong71 <wotkd2647@naver.com>
2025-10-23 15:58:59 +0900

    refactor : RestaurantRepo QUERY

--- src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
+++ src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
@@ -3,11 +3,18 @@ package com.example.nawa6practice.repository;
 import com.example.nawa6practice.domain.Restaurant;
 import com.example.nawa6practice.domain.RestaurantCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
+    @Query("""
+        SELECT R
+        FROM Restaurant R
+        WHERE r.category = :category AND r.status = 'ACTIVE'
+        ORDER BY r.category DESC
+""")
     List<Restaurant> findByCategory(RestaurantCategory category);
 
     Long id(Long id);
