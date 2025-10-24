afb49ec0a2b2334f69b3a5a5672c6e6c56b260f4
majayong71 <wotkd2647@naver.com>
2025-10-24 17:17:11 +0900

    refactor : User QUERY Exclude Save Query

--- src/main/java/com/example/nawa6practice/repository/UserRepository.java
+++ src/main/java/com/example/nawa6practice/repository/UserRepository.java
@@ -1,11 +1,32 @@
 package com.example.nawa6practice.repository;
 
 import com.example.nawa6practice.domain.User;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     /**
      * 데이터가 존재하면 true , 없으면 false boolean 형태의 값을 반환하는 쿼리 메서드.
      */
+    @Query("""
+        SELECT COUNT(U) > 0
+        FROM User U
+        WHERE u.email = :email AND u.status = 'ACTIVE'
+        ORDER BY u.id desc
+""")
     boolean existsByEmail(String email);
+
+    @NonNull
+    @Query("""
+        SELECT U
+        FROM User u
+        WHERE u.id = :id AND u.status = 'ACTIVE'
+        ORDER BY u.id DESC
+""")
+    Optional<User> findById(@NonNull Long id);
+
+
 }
