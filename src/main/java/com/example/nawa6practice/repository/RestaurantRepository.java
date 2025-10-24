7ef0003219d4a49aa245218346d3bb5adfe9b05f
majayong71 <wotkd2647@naver.com>
2025-10-24 15:17:38 +0900

    refactor : RestaurantRepo QUERY

--- src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
+++ src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
@@ -2,21 +2,39 @@ package com.example.nawa6practice.repository;
 
 import com.example.nawa6practice.domain.Restaurant;
 import com.example.nawa6practice.domain.RestaurantCategory;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     @Query("""
-        SELECT R
-        FROM Restaurant R
-        WHERE r.category = :category AND r.status = 'ACTIVE'
-        ORDER BY r.category DESC
-""")
+                    SELECT R
+                    FROM Restaurant R
+                    WHERE r.category = :category AND r.status = 'ACTIVE'
+                    ORDER BY r.category DESC
+            """)
     List<Restaurant> findByCategory(RestaurantCategory category);
 
-    Long id(Long id);
+    @NonNull
+    @Query("""
+                    SELECT R
+                    FROM Restaurant R
+                    WHERE r.id = :id AND r.status = 'ACTIVE'
+                    ORDER BY r.id DESC
+            """)
+    Optional<Restaurant> findById(@NonNull Long id);
+
+    @NonNull
+    @Query("""
+                    SELECT R
+                    FROM Restaurant R
+                    WHERE r.status = 'ACTIVE'
+                    ORDER BY r.id DESC
+            """)
+    List<Restaurant> findAll();
 
 }
