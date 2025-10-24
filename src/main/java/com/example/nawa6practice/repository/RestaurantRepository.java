d4e95723789244e4165e7a5e482cb4da1286fc78
majayong71 <wotkd2647@naver.com>
2025-10-24 15:18:16 +0900

    refactor : RestaurantRepo QUERY Exclude Save Query

--- src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
+++ src/main/java/com/example/nawa6practice/repository/RestaurantRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
+
     @Query("""
                     SELECT R
                     FROM Restaurant R
