b2a3a49325ffc14c37a13ce3fda66e62bd355642
majayong71 <wotkd2647@naver.com>
2025-10-27 19:30:56 +0900

    refactor : User QUERY Exclude Save Query

--- src/main/java/com/example/nawa6practice/repository/UserRepository.java
+++ src/main/java/com/example/nawa6practice/repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     /**
      * 데이터가 존재하면 true , 없으면 false boolean 형태의 값을 반환하는 쿼리 메서드.
      */
